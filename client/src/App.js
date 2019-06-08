e07915a4e194bc26657a30480b31c8bcb8bb7239
Ashish <ashishthesatan@gmail.com>
2019-06-08 20:30:11 +0545

    added frontend routing

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,22 @@ import "./App.css";
 import Navbar from "./components/layouts/Navbar";
 import Landing from "./components/layouts/Landing";
 import Footer from "./components/layouts/Footer";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Landing />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <div className="container">
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
