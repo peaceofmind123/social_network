7f3345d41cdde10fccceba2fa405d2dd59e05d49
Ashish <ashishthesatan@gmail.com>
2019-07-05 21:35:24 +0545

    removed the github redirect bug

--- client/src/components/profile/ProfileHeader.js
+++ client/src/components/profile/ProfileHeader.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import isEmpty from '../../validation/is-empty';
+import React, { Component } from "react";
+import isEmpty from "../../validation/is-empty";
 
 class ProfileHeader extends Component {
   render() {
@@ -21,7 +21,7 @@ class ProfileHeader extends Component {
             <div className="text-center">
               <h1 className="display-4 text-center">{profile.user.name}</h1>
               <p className="lead text-center">
-                {profile.status}{' '}
+                {profile.status}{" "}
                 {isEmpty(profile.company) ? null : (
                   <span>at {profile.company}</span>
                 )}
@@ -31,7 +31,7 @@ class ProfileHeader extends Component {
                 {isEmpty(profile.website) ? null : (
                   <a
                     className="text-white p-2"
-                    href={profile.website}
+                    href={`http://${profile.website}`}
                     target="_blank"
                   >
                     <i className="fas fa-globe fa-2x" />
@@ -41,7 +41,7 @@ class ProfileHeader extends Component {
                 {isEmpty(profile.social && profile.social.twitter) ? null : (
                   <a
                     className="text-white p-2"
-                    href={profile.social.twitter}
+                    href={`http://${profile.social.twitter}`}
                     target="_blank"
                   >
                     <i className="fab fa-twitter fa-2x" />
@@ -51,7 +51,7 @@ class ProfileHeader extends Component {
                 {isEmpty(profile.social && profile.social.facebook) ? null : (
                   <a
                     className="text-white p-2"
-                    href={profile.social.facebook}
+                    href={`http://${profile.social.facebook}`}
                     target="_blank"
                   >
                     <i className="fab fa-facebook fa-2x" />
@@ -61,7 +61,7 @@ class ProfileHeader extends Component {
                 {isEmpty(profile.social && profile.social.linkedin) ? null : (
                   <a
                     className="text-white p-2"
-                    href={profile.social.linkedin}
+                    href={`http://${profile.social.linkedin}`}
                     target="_blank"
                   >
                     <i className="fab fa-linkedin fa-2x" />
@@ -71,7 +71,7 @@ class ProfileHeader extends Component {
                 {isEmpty(profile.social && profile.social.youtube) ? null : (
                   <a
                     className="text-white p-2"
-                    href={profile.social.youtube}
+                    href={`http://${profile.social.youtube}`}
                     target="_blank"
                   >
                     <i className="fab fa-youtube fa-2x" />
@@ -81,7 +81,7 @@ class ProfileHeader extends Component {
                 {isEmpty(profile.social && profile.social.instagram) ? null : (
                   <a
                     className="text-white p-2"
-                    href={profile.social.instagram}
+                    href={`http://${profile.social.instagram}`}
                     target="_blank"
                   >
                     <i className="fab fa-instagram fa-2x" />
